e8dfbe6afb68ad604fd572899ef38b6d7266b23a
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-02 23:29:15 +0200

    Bump Community Gadget for LAN ringing build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "za.co.cyberpulse.communitygadget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0-prototype"
+        versionCode = 2
+        versionName = "1.0.1-lan-ring"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
